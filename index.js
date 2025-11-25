29e2d185119e277795953340ff76bab382f78ec2
Asadullah Al Jihad <itsjihad@proton.me>
2025-11-25 18:23:32 +0600

    connected mongoDB

--- index.js
+++ index.js
@@ -1,16 +1,51 @@
-const express = require('express');
-const app =express()
-const cors = require('cors');
-const port = process.env.PORT|| 3000
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 3000;
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.xiyzyju.mongodb.net/?appName=Cluster0;`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send("server is up")
-})
+async function run() {
+  try {
+    await client.connect();
 
-app.listen(port,()=>{
-    console.log("listening at",port);
     
-})
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    app.listen(port, () => {
+      console.log("listening at", port);
+    });
+  }
+}
+run().catch(console.dir);
